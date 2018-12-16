a285f94d35c24a759b34a7b5fe53d000962f9000
wellingtonx <wellingtonex@gmail.com>
2018-12-16 20:55:05 -0200

    implementando exclusão de lancamentos

--- src/main/java/com/algaworks/algamoney/dois/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/dois/api/resource/LancamentoResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,11 @@ public class LancamentoResource {
 		lancamento = service.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamento.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamento);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
 	}
 }
