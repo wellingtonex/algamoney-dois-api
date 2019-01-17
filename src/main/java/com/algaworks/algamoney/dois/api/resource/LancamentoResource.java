57ed28619b94deec4b90ccd945522c1ed97e3d33
wellingtonx <wellingtonex@gmail.com>
2019-01-16 23:38:07 -0200

    implementando uploade de anexos de lancamentos

--- src/main/java/com/algaworks/algamoney/dois/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/dois/api/resource/LancamentoResource.java
@@ -1,5 +1,8 @@
 package com.algaworks.algamoney.dois.api.resource;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.algamoney.dois.api.dto.LancamentoEstatisticaCategoriaDTO;
 import com.algaworks.algamoney.dois.api.dto.LancamentoEstatisticaDiaDTO;
@@ -146,7 +150,14 @@ public class LancamentoResource {
 				.body(relatorio);
 	}
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+	@PostMapping("/anexo")
+	public String uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
+		OutputStream out = new FileOutputStream("/home/wellington/Documentos/anexo--" + anexo.getOriginalFilename());
+		out.write(anexo.getBytes());
+		out.close();
+		return "ok";
+	}
 	
 	
 	
